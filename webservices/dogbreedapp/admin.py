6cdc0ec4b7c717f50a1747bebef9685b1c363549
Geoffrey Humphreys <ghumphreys@unomaha.edu>
2024-10-05 06:49:35 -0400

    Created admin interface

--- webservices/dogbreedapp/admin.py
+++ webservices/dogbreedapp/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Dog
+from .models import Breed
 
 # Register your models here.
+
+# adapted from https://mlhale.github.io/CYBR8470/modules/webservices/
+class DogAdmin(admin.ModelAdmin):
+    # Define the list of fields to display in the admin interface
+    list_display = ('name', 'age', 'breed', 'gender', 'color', 'favoritefood', 'favoritetoy',)
+    
+    # Add search functionality for specific fields
+    search_fields = ('name', 'breed',)
+
+    # Add filters for the age and breed fields in the sidebar
+    list_filter = ('age', 'breed',)
+
+    # Define which fields can be clicked to view the details page
+    list_display_links = ('name',)
+
+    # Define how fields are displayed when editing a Dog instance
+    fields = ('name', 'age', 'breed', 'gender', 'color', 'favoritefood', 'favoritetoy',)
+
+# Register the model and admin class
+admin.site.register(Dog, DogAdmin)
+
+class BreedAdmin(admin.ModelAdmin):
+    # Define the list of fields to display in the admin interface
+    list_display = ('name', 'size', 'friendliness', 'trainability', 'sheddingamount', 'exerciseneeds',)
+    
+    # Add search functionality for specific fields
+    search_fields = ('name', 'size', 'friendliness', 'trainability', 'sheddingamount', 'exerciseneeds',)
+
+    # Add filters for the age and breed fields in the sidebar
+    list_filter = ('name', 'size', 'friendliness', 'trainability', 'sheddingamount', 'exerciseneeds',)
+
+    # Define which fields can be clicked to view the details page
+    list_display_links = ('name',)
+
+    # Define how fields are displayed when editing a Breed instance
+    fields = ('name', 'size', 'friendliness', 'trainability', 'sheddingamount', 'exerciseneeds',)
+
+# Register the model and admin class
+admin.site.register(Breed, BreedAdmin)
+
--- webservices/dogbreedapp/models.py
+++ webservices/dogbreedapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
+
 
 # Create your models here.
+
+# adapted from https://mlhale.github.io/CYBR8470/modules/webservices/
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    favoritefood = models.CharField(max_length=100)
+    favoritetoy = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Breed(models.Model):
+    name = models.CharField(max_length=100)
+    size = models.CharField(max_length=6, choices=[(a, a) for a in ("Tiny", "Small", "Medium", "Large")])
+    friendliness = models.IntegerField(validators=(MaxValueValidator(5), MinValueValidator(1)), choices=[(a, a) for a in range(1, 6)])
+    trainability = models.IntegerField(validators=(MaxValueValidator(5), MinValueValidator(1)), choices=[(a, a) for a in range(1, 6)])
+    sheddingamount = models.IntegerField(validators=(MaxValueValidator(5), MinValueValidator(1)), choices=[(a, a) for a in range(1, 6)])
+    exerciseneeds = models.IntegerField(validators=(MaxValueValidator(5), MinValueValidator(1)), choices=[(a, a) for a in range(1, 6)])
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
